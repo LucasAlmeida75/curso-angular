d529924be946dfc8017b90d07de7376474ece2c1
LucasAlmeida75 <lucasalmeidamachado1@gmail.com>
2025-01-22 08:32:11 -0300

    feat: diretivas do angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
+import { DirectivesComponent } from './components/directives/directives.component';
 
 @Component({
   selector: 'app-root',
-  imports: [FirstComponentComponent, ParentDataComponent],
+  imports: [FirstComponentComponent, ParentDataComponent, DirectivesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/directives/directives.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-directives',
+  imports: [CommonModule],
+  templateUrl: './directives.component.html',
+  styleUrl: './directives.component.css'
+})
+export class DirectivesComponent {
+  size    = 40;
+  font    = 'Arial';
+  color   = 'red';
+  classes = ['green-title', 'small-title'];
+}
