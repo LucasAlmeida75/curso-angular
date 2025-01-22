eb9193604e28edd7ccd00216b550c722c3b3dcc8
LucasAlmeida75 <lucasalmeidamachado1@gmail.com>
2025-01-22 08:58:18 -0300

    feat: if-render

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
 import { DirectivesComponent } from './components/directives/directives.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
 
 @Component({
   selector: 'app-root',
-  imports: [FirstComponentComponent, ParentDataComponent, DirectivesComponent],
+  imports: [FirstComponentComponent, ParentDataComponent, DirectivesComponent, IfRenderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
