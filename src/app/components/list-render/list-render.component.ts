d02682243989b45735add1b937fc88217872a38c
LucasAlmeida75 <lucasalmeidamachado1@gmail.com>
2025-01-24 08:40:03 -0300

    feat: interface

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
+import { Animal } from '../../Animal';
 
 @Component({
   selector: 'app-list-render',
@@ -9,10 +10,16 @@ import { Component } from '@angular/core';
   styleUrl: './list-render.component.css'
 })
 export class ListRenderComponent {
-  animals = [
-    {name: 'Turca', type: 'dog'},
-    {name: 'Tom', type: 'cat'},
-    {name: 'Frida', type: 'dog'},
-    {name: 'Bob', type: 'horse'}
+  animals: Animal[] = [
+    {name: 'Turca', type: 'dog', age: 2},
+    {name: 'Tom', type: 'cat', age: 5},
+    {name: 'Frida', type: 'dog', age: 3},
+    {name: 'Bob', type: 'horse', age: 1}
   ]
+
+  animalDetails = '';
+
+  showAge(animal: Animal): void {
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos!`;
+  }
 }
